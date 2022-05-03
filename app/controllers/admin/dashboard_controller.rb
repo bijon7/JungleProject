da73d521d91c77333fb4894a49940489d63f5340
Zubaer <bijon7@yahoo.com>
2022-05-03 10:34:48 -0700

    Defines show method to count products and categories in dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: 'username', password: 'password'
-
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+# Defines method to count number of products and categories through
+# instance variables and passes to show.html.erb.
   def show
+    @products=Product.count
+    @categories=Category.count
   end
 end
